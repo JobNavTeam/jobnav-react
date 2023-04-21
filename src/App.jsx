c9322ea27947328d5aba0335694dd8d7b13118e9
Robert Jones <JonesBrendanDev@gmail.com>
2023-04-20 22:21:15 -0500

    fixed styles bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import '../src/sass/style.css'
 
 // Imports
 import Navbar from '../src/html/layout/navbar'
--- src/html/layout/navbar.jsx
+++ src/html/layout/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <header className="primary-header">
-      Hello
+      Why is my CSS not working
     </header>
   )
 }
